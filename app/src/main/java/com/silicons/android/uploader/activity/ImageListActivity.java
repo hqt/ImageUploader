3ff405b6234cf53a819e8c0568006f2e454db260
Huynh Quang Thao <huynhquangthao@gmail.com>
2015-12-30 13:43:31 +0700

    add layout resource file for activity

--- app/src/main/java/com/silicons/android/uploader/activity/ImageListActivity.java
+++ app/src/main/java/com/silicons/android/uploader/activity/ImageListActivity.java
@@ -1,10 +1,52 @@
 package com.silicons.android.uploader.activity;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.silicons.android.uploader.R;
 
 /**
  * Created by Huynh Quang Thao on 12/30/15.
  */
 public class ImageListActivity extends AppCompatActivity {
+
+    private FloatingActionButton photoUploadButton;
+    private FloatingActionButton cameraUploadButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_list);
+
+        // set toolbar option
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_camera);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        photoUploadButton = (FloatingActionButton) findViewById(R.id.fab_photos);
+        cameraUploadButton = (FloatingActionButton) findViewById(R.id.fab_camera);
+
+        photoUploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cameraUploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
 
 }
---
+++ app/src/main/java/com/silicons/android/uploader/activity/PhotoViewerActivity.java
@@ -0,0 +1,19 @@
+package com.silicons.android.uploader.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.silicons.android.uploader.R;
+
+/**
+ * Created by Huynh Quang Thao on 12/30/15.
+ */
+public class PhotoViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_list);
+    }
+
+}
